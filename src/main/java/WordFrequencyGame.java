659a0b13b616e4536b41dffd46237ce55ae56dde
Gloria Chan <changl3@oocl.com>
2021-12-06 17:47:43 +0800

    [Refactoring][Gloria] -refactor: edit method getResult and add method getString

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -7,43 +7,14 @@ public class WordFrequencyGame {
     public static final String SPACE_PATTERN = "\\s+";
 
     public String getResult(String sentence){
-
-
-        if (isSingleWord(sentence)) {
-            return sentence + " 1";
-        } else {
-
-            try {
-
-                //split the input string with 1 to n pieces of spaces
-                List<WordInfo> wordInfoList = getWordInfoList(sentence);
-
-                //get the map for the next step of sizing the same word
-                Map<String, List<WordInfo>> map =getListMap(wordInfoList);
-
-                List<WordInfo> list = new ArrayList<>();
-                for (Map.Entry<String, List<WordInfo>> entry : map.entrySet()){
-                    WordInfo wordInfo = new WordInfo(entry.getKey(), entry.getValue().size());
-                    list.add(wordInfo);
-                }
-                wordInfoList = list;
-
-                wordInfoList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
-                StringJoiner joiner = new StringJoiner("\n");
-                for (WordInfo w : wordInfoList) {
-                    String s = w.getValue() + " " +w.getWordCount();
-                    joiner.add(s);
-                }
-                return joiner.toString();
-            } catch (Exception e) {
-
-
-                return "Calculate Error";
-            }
+        try {
+            List<WordInfo> wordInfoList = getWordInfoList(sentence);
+            wordInfoList.sort((word1, word2) -> word2.getWordCount() - word1.getWordCount());
+            return getString(wordInfoList);
+        } catch (Exception e) {
+            return "Calculate Error";
         }
     }
-
     private List<WordInfo> getWordInfoList(String sentence) {
         List<String> words = Arrays.asList(sentence.split(SPACE_PATTERN));
         List<String> distinctWords = words.stream().distinct().collect(Collectors.toList());
@@ -56,28 +27,13 @@ public class WordFrequencyGame {
         return wordInfos;
     }
 
-
-    private Map<String,List<WordInfo>> getListMap(List<WordInfo> wordInfoList) {
-        Map<String, List<WordInfo>> map = new HashMap<>();
-        for (WordInfo wordInfo : wordInfoList){
-//       map.computeIfAbsent(wordInfo.getValue(), k -> new ArrayList<>()).add(wordInfo);
-            if (!map.containsKey(wordInfo.getValue())){
-                ArrayList arr = new ArrayList<>();
-                arr.add(wordInfo);
-                map.put(wordInfo.getValue(), arr);
-            }
-
-            else {
-                map.get(wordInfo.getValue()).add(wordInfo);
-            }
+    private String getString(List<WordInfo> wordInfoList) {
+        StringJoiner joiner = new StringJoiner("\n");
+        for (WordInfo wordInfo : wordInfoList) {
+            String word = wordInfo.getValue()+" "+wordInfo.getWordCount();
+            joiner.add(word);
         }
-
-
-        return map;
-    }
-
-    private boolean isSingleWord(String sentence) {
-        return sentence.split(SPACE_PATTERN).length==1;
+        return joiner.toString();
     }
     
 
